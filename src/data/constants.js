943c6a44194b561f61f5d2bb2aa415c802b37847
Ravi Prasad <Raviprasad53339@gmail.com>
2025-04-30 00:56:01 +0530

    Add files via upload

---
+++ src/data/constants.js
@@ -0,0 +1,290 @@
+export const Bio = {
+  name: "Ravi Prasad",
+  roles: [
+    "Full Stack Developer",
+    "Android Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a passionate and adaptable individual, driven by a constant desire to learn and grow. I take pride in delivering high-quality work and embracing new challenges with enthusiasm. With a positive outlook and a commitment to continuous improvement, I am eager to contribute meaningfully and achieve outstanding results.",
+  github: "https://github.com/Raviprasad53339",
+  resume:
+    "https://drive.google.com/file/d/15L-Q3ZAJ4CjlaI1awpdmReEW67Bl5IO3/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/ravi-prasad-a82067279/",
+  twitter: "https://x.com/Ravi9653621614",
+  insta: "https://www.instagram.com/ravi_r_prasad/",
+  youtube: "https://www.youtube.com/@Raviprasadmotovlog/videos",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Flutter",
+        image:
+          "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "Android",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "Kotlin",
+        image:
+          "https://www.vectorlogo.zone/logos/kotlinlang/kotlinlang-icon.svg",
+      },
+      {
+        name: "XML",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBMw6_RdwKQ9bDFfnKDX1iwMl4bVJEvd9PP53XuIw&s",
+      },
+      {
+        name: "Android Studio",
+        image:
+          "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 3,
+    img: "https://raw.githubusercontent.com/Raviprasad53339/Ravi-Prasad-portfolio/refs/heads/main/images/Appnity.jpg", 
+    role: "Android Developer Intern",
+    company: "Appnity ",
+    date: "April 2024 - Jan 2025",
+    desc: "At Appnity, I worked as an Android Developer Intern focused on interface design and feature implementation tailored to diverse client requirements. My role involved designing intuitive mobile interfaces, integrating backend services, and ensuring seamless user experiences across Android devices. I collaborated with cross-functional teams to deliver high-quality apps using Java and XML, while also working with databases like Firebase, SQLite, and MongoDB. Additionally, I handled RESTful API integration and contributed to improving overall app architecture.",
+    skills: [
+      "Leadership",
+      "Mobile Application Development",
+      "Kotlin",
+      "XML",
+      "Java",
+      "Firebase",
+      "SQLite",
+      "MongoDB",
+      "RESTful API",
+    ],
+  },
+  {
+    id: 4,
+    img: "https://raw.githubusercontent.com/Raviprasad53339/Ravi-Prasad-portfolio/refs/heads/main/images/webteck.jpg",    
+    role: "Frontend Developer Intern",
+    company: "Webtech ",
+    date: "Sep 2023 - Feb 2024",
+    desc: "Working as a Frontend Developer at Webteck, I'm responsible for designing and developing responsive, user-friendly websites tailored to client specifications. I collaborate closely with designers and backend developers to translate UI/UX mockups into pixel-perfect interfaces using modern web technologies like HTML5, CSS3, JavaScript, and React. I’ve contributed to several client projects, ensuring fast load times, accessibility, and cross-browser compatibility. This role has significantly strengthened my skills in frontend development, version control (Git), and agile workflows while deepening my understanding of client-centric design and development.",
+    skills: [
+      "JavaScript",
+      "HTML",
+      "CSS",
+      "Firebase",
+      "SQLite",
+      "React.js",
+      "Python",
+      "API",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://raw.githubusercontent.com/Raviprasad53339/Ravi-Prasad-portfolio/refs/heads/main/images/kvpendaarkar%20(1).jpeg",
+    school: "K. V. Pendharkar College of Arts, Science and Commerce",
+    date: "June 2022 - May 2025",
+    grade: "0.00 CGPA",
+    desc: "I completed my Bachelor's degree in Computer Science at K. V. Pendharkar College of Arts, Science and Commerce, successfully finishing all 6 semesters with a CGPA of 0.00. My coursework covered a wide range of subjects including Programming (C, Python, Java), Data Structures and Algorithms, Object-Oriented Programming, Operating Systems, Database Management Systems, Computer Networks, Web Technologies, Software Engineering, Computer Architecture, Digital Electronics, Discrete Mathematics, Mobile App Development, Cloud Computing, Artificial Intelligence, Cybersecurity, and Project Work, among others.",
+    degree: "Bachelor of Science - BSC-CS, Computer Science",
+  },
+  {
+    id: 1,
+    img: "https://raw.githubusercontent.com/Raviprasad53339/Ravi-Prasad-portfolio/refs/heads/main/images/iraglobal%20(1).jpeg",
+    school: "Global jr. College of Science & Commerce",
+    date: "June 2018 - February 2020",
+    grade: "46.92%",
+    desc: "I completed my class 12 high school education at Global jr. College of Science & Commerce, Katai-Kole, where I studied Science with Mathematics.",
+    degree: "HSC(XII), Science with Mathematics",
+  },
+  {
+    id: 2,
+    img: "https://raw.githubusercontent.com/Raviprasad53339/Ravi-Prasad-portfolio/refs/heads/main/images/sakharam%20(1).jpeg",
+    school: "Sakharam Sheth Vidyalaya Katai-Kole",
+    date: "March 2018",
+    grade: "55.60%",
+    desc: "I completed my class 10 education at Sakharam Sheth Vidyalaya, Katai-kole, where I studied Science with Mathematics.",
+    degree: "SSC(X), Science with Mathematics",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "TradewithRavi",
+    date: "Jun 2024 - Jul 2024",
+    description:
+      "Developed an interactive web platform aimed at teaching users stock market concepts through structured lessons, quizzes, and real-time mock trading simulations. The platform is designed to support both beginners and intermediate traders with a step-by-step learning approach. Implemented features like dynamic data visualization for stock trends, personalized learning paths, and AI-powered chatbot support for real-time user assistance. Ensured the platform is responsive, user-friendly, and scalable.",
+    image:
+      "https://raw.githubusercontent.com/Raviprasad53339/Tradewithravi/refs/heads/main/image.png",
+    tags: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "React Js",
+      "Firebase",
+      "Chart.js",
+      "Chatbase",
+      "REST APIs",
+    ],
+    category: "web app",
+    github: "https://github.com/Raviprasad53339/Tradewithravi",
+    webapp: "https://tradewithravi.netlify.app/",
+  },
+  {
+    id: 3,
+    title: "Crack It Interview Question Generator Chatbot — Android Application",
+    date: "January 2025 - February 2025",
+    description:
+      "Developed an Android application that dynamically generates interview questions based on any topic entered by the user, enhancing personalized interview preparation. Integrated Google Gemini API for AI-driven question generation. Implemented a secure login and signup system using SQLite for local data storage. Focused on creating a clean, user-friendly interface in Android Studio, ensuring smooth user experience and offline functionality for authentication.",
+    image:
+      "https://raw.githubusercontent.com/Raviprasad53339/Crack-It---Interview-Question-Generator/refs/heads/main/Crack%20It%20images.jpg",
+    tags: ["Kotlin", "Java", "XML", "Android Studio", "SQLite", "Google Gemini API"],
+    category: "android app",
+    github: "https://github.com/Raviprasad53339/Crack-It---Interview-Question-Generator",
+    webapp: "https://github.com/Raviprasad53339/Crack-It---Interview-Question-Generator/blob/main/Crack%20It.apk",
+  },
+  {
+    id: 10,
+    title: "FarmerFriend",
+    date: "Aug 2024 - Sep 2024",
+    description:
+      "Developed a mobile application that enables farmers to share, rent, or exchange agricultural tools and machinery with nearby farmers, promoting cost savings and efficient resource utilization. Integrated Razorpay payment gateway to facilitate secure and seamless transactions between farmers for equipment rentals. The app features user authentication, real-time equipment listings, location-based services using Google Maps API, and smooth communication between users. Focused on delivering a user-friendly interface and a robust backend to improve accessibility in rural areas.",
+    image:
+      "https://raw.githubusercontent.com/Raviprasad53339/FarmerFriend/refs/heads/main/FarmerFriend%20Images.jpg",
+    tags: ["Kotlin", "Java", "XML", "Firebase", "Android Studio", "Razorpay", "Google Maps API", "SQLite"],
+    category: "android app",
+    github: "https://github.com/Raviprasad53339/FarmerFriend",
+    webapp: "https://github.com/Raviprasad53339/FarmerFriend/blob/main/FarmerFriend.apk",
+  },
+  {
+    id: 6,
+    title: "Job Interview Question Generator Chatbot",
+    date: "January 2025 - February 2025",
+    description:
+      "Designed and developed a chatbot that dynamically generates customized interview questions based on the user's selected domain (e.g., Web Development, Data Science, Networking), helping students and job seekers enhance their interview preparation. Integrated Google Gemini API for generating domain-specific questions, and a responsive front-end using HTML, CSS, and JavaScript to ensure a seamless user experience.",
+    image:
+      "https://raw.githubusercontent.com/Raviprasad53339/Job-Interview-Question-Generator-Chatbot/refs/heads/main/Screenshot%20(199).png",
+    tags: ["Html", "CSS", "JavaScript", "Google Gemini API"],
+    category: "web app",
+    github: "https://github.com/Raviprasad53339/Job-Interview-Question-Generator-Chatbot",
+    webapp: "https://jiqgc.netlify.app",
+  },
+  {
+    id: 7,
+    title: "Healthcare Chatbot - AI Medical Assistant",
+    date: "January 2025 - February 2025",
+    description:
+      "Built an AI-powered chatbot designed to provide basic healthcare advice, symptom checking, and medication information using natural language processing techniques. Integrated Google Gemini API for advanced conversational capabilities, Firebase for real-time user data management, and SQLite for efficient local data storage. Developed a user-friendly Android app with fast performance, ensuring quick access to healthcare information even in offline scenarios.",
+    image:
+      "https://raw.githubusercontent.com/Raviprasad53339/Healthcare-Chatbot/refs/heads/main/Healthcare%20Chatbot%20image.jpg",
+    tags: ["Java", "XML", "Google Gemini API", "Android Studio", "Firebase", "SQLite"],
+    category: "android app",
+    github: "https://github.com/Raviprasad53339/Healthcare-Chatbot/blob/main/HealthChatbot.zip",
+    webapp: "https://github.com/Raviprasad53339/Healthcare-Chatbot/blob/main/Healthcare%20Chatbot.apk",
+  },
+  {
+    id: 8,
+    title: "AutoAlgo",
+    date: "Oct 2024 - Dec 2024",
+    description:
+      "Developed an automated trading algorithm that executes buy and sell orders based on predefined strategies and real-time market data, minimizing human error and optimizing trade entry and exit points. Implemented robust data analysis techniques for signal generation, backtesting strategies, and visualizing trading performance. Integrated Fyers API for live trading execution and market data fetching. Focused on improving trade accuracy, efficiency, and portfolio management automation.",
+    image:
+      "https://raw.githubusercontent.com/Raviprasad53339/AutoAlgo/refs/heads/main/AutoAlgo.jpg",
+    tags: ["Python", "Pandas", "NumPy", "Fyers API", "Matplotlib", ],
+    category: "machine learning",
+    github: "https://github.com/Raviprasad53339/AutoAlgo",
+    webapp: "https://github.com/Raviprasad53339/AutoAlgo",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
